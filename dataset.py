1e65206dae32ea0263ecfb3b5c6cc2e7100c2d87
Rael <santosrafaelpro@gmail.com>
2025-03-13 00:20:07 -0300

    site

--- dataset.py
+++ dataset.py
@@ -3,3 +3,5 @@ import kaggle
 kaggle.api.authenticate()
 
 kaggle.api.dataset_download_files('najir0123/walmart-10k-sales-datasets', path='.', unzip=True)
+
+# Site to download the dataset: https://www.kaggle.com/najir0123/walmart-10k-sales-datasets
